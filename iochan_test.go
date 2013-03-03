c8ba35b26662fb8e08d374d73c7d0bf76d62c321
jszwedko <jesse.szwedko@gmail.com>
2013-03-03 18:36:45 -0500

    Adding error chan to API
    So that the caller can see the error returned by the underlying reader
    when the string channel is closed (usually this will be an io.EOF).

--- iochan_test.go
+++ iochan_test.go
@@ -3,18 +3,27 @@ package iochan
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"testing"
 )
 
 func TestBasic(t *testing.T) {
 	b := make(Buffer, 10)
 	r := bytes.NewBuffer([]byte("this is  a buffer withlongwords sometimes"))
-	for s := range b.ReaderChan(r, " ") {
+	words, errs := b.ReaderChan(r, " ")
+	for s := range words {
 		fmt.Printf("%s|", s)
+	}
+	if err := <-errs; err != nil && err != io.EOF {
+		t.Error(err)
 	}
 
 	b = make(Buffer, 1024)
-	for s := range b.FileLineChan("iochan_test.go") {
+	lines, errs := b.FileLineChan("iochan_test.go")
+	for s := range lines {
 		fmt.Print(s)
+	}
+	if err := <-errs; err != nil && err != io.EOF {
+		t.Error(err)
 	}
 }
